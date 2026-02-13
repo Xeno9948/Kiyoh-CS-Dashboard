0b91bb2a6aafade8533fe9d70060efc905a746b0
Xeno9948 <stathisxeno@icloud.com>
2026-02-13 14:31:14 +0200

    Fix deployment: remove Prisma engineType and add Emotion cache for MUI SSR

--- src/components/ThemeRegistry.tsx
+++ src/components/ThemeRegistry.tsx
@@ -1,14 +1,22 @@
 'use client';
 
+import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 import { theme } from '@/lib/theme';
+
+// Create emotion cache
+const cache = createCache({ key: 'css', prepend: true });
 
 export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            {children}
-        </ThemeProvider>
+        <CacheProvider value={cache}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {children}
+            </ThemeProvider>
+        </CacheProvider>
     );
 }
