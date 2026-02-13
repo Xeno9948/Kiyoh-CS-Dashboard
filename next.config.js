a8c8d4a807599a8e2fb27c8479fb0329093c1a8d
Xeno9948 <stathisxeno@icloud.com>
2026-02-13 14:48:36 +0200

    Configure Next.js standalone output to fix static generation errors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
+  // Disable static optimization to prevent build-time errors
+  experimental: {
+    isrMemoryCacheSize: 0,
+  },
 }
 
 module.exports = nextConfig
